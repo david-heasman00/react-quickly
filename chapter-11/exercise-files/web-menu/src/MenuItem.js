b7e32fbcd41a199c8d065b49e088685d7e05dd5a
David Heasman <1979286+david-heasman00@users.noreply.github.com>
2026-09-10 14:40:08 +0100

    Begin chapter 11 exercises - building a dynamic web menu

---
+++ chapter-11/exercise-files/web-menu/src/MenuItem.js
@@ -0,0 +1,12 @@
+function MenuItem({href, icon, children}) {
+    const iconSrc = `/icons/${icon}.svg`;
+    return(
+        <li>
+            <a href={href} className="menu-link">
+                <img src={iconSrc} width="16" alt="" />
+                {children}
+            </a>
+        </li>
+    );
+}
+export default MenuItem;
